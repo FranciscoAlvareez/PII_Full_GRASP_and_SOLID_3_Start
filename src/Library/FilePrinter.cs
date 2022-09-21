20c9655022af77e422550ab6f1dd6c20d2556d4e
FranciscoAlvareez <fran.alvareez04@gmail.com>
2022-09-21 11:24:25 -0300

    Solucion

---
+++ src/Library/FilePrinter.cs
@@ -0,0 +1,8 @@
+using System.IO;
+namespace Full_GRASP_And_SOLID.Library;
+public class FilePrinter: IPrinter{
+    public void PrintTicket(Recipe recipe)
+    {
+        File.WriteAllText("Ticket.txt", recipe.GetTextToPrint());
+    }
+}
